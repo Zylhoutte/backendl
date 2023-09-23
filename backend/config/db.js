d9229735a7631975074d66a5df470651100b1d34
Zylhoutte <gianpilla9@gmail.com>
2023-09-23 16:51:44 +0800

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // Add your options here
+      strictQuery: true,
+      // Add other options if needed
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
