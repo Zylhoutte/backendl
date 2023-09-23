fbaccbc570dcc4e88f506b7a8681d948c9687f3e
Zylhoutte <gianpilla9@gmail.com>
2023-09-23 16:58:43 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', true); // Set strictQuery option here
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
